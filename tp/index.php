bf0adc3fd79878a39140734b64085a882fce0827
v_liutiecheng <v_liutiecheng@xdf.cn>
2016-11-28 12:04:10 +0800

    +++@author+++

--- tp/index.php
+++ tp/index.php
@@ -10,7 +10,7 @@
 // +----------------------------------------------------------------------
 
 // 应用入口文件
-//项目@author
+//项目@authorliutiecheng
 // 检测PHP环境
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
